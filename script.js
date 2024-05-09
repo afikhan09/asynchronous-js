a9dde52e27153531798a5719063516158540ba71
afikhan.javaventures@gmail.com <Afikhan123$>
2024-05-09 21:08:52 +0530

    callback hell

--- script.js
+++ script.js
@@ -33,5 +33,55 @@ const getcountryData = function (country) {
   });
 };
 
-getcountryData('portugal');
-getcountryData('usa');
+// getcountryData('portugal');
+// getcountryData('usa');
+
+const renderCountry = function (data, className = '') {
+  const html = ` 
+  <article class="country ${className} ">
+          <img class="country__img" src="${data.flag}" />
+          <div class="country__data">
+            <h3 class="country__name">${data.name}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${(
+              +data.population / 1000000
+            ).toFixed(1)} people</p>
+            <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+            <p class="country__row"><span>💰</span>${
+              data.currencies[0].name
+            }</p>
+          </div>
+        </article>`;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+const getcountryAndNeighbour = function (country) {
+  //ajax call country1
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v2/name/${country}`);
+  request.send();
+  console.log(request.responseText);
+
+  request.addEventListener('load', function () {
+    //   console.log(this.responseText); // here this kyword refers to the request
+    const [data] = JSON.parse(this.responseText);
+    //render country 1
+    renderCountry(data);
+
+    //get neighbour country 2
+    const neighbour = data.borders?.[0];
+
+    //ajax call country 2
+
+    const request2 = new XMLHttpRequest();
+    request2.open('GET', `https://restcountries.com/v2/alpha/${neighbour}`);
+    request2.send();
+
+    request2.addEventListener('load', function () {
+      console.log(this.responseText);
+      const data2 = JSON.parse(this.responseText);
+      renderCountry(data2, 'neighbour');
+    });
+  });
+};
+getcountryAndNeighbour('portugal');
